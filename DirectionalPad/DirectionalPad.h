0c1e6e2c36af9ae2f6f15f08986daeec0679d928
Aaron Sutton <aaronjsutton@icloud.com>
2018-05-22 20:52:53 -0400

    Initial Commit

---
+++ DirectionalPad/DirectionalPad.h
@@ -0,0 +1,19 @@
+//
+//  DirectionalPad.h
+//  DirectionalPad
+//
+//  Created by Aaron Sutton on 5/22/18.
+//  Copyright © 2018 Aaron Sutton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DirectionalPad.
+FOUNDATION_EXPORT double DirectionalPadVersionNumber;
+
+//! Project version string for DirectionalPad.
+FOUNDATION_EXPORT const unsigned char DirectionalPadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DirectionalPad/PublicHeader.h>
+
+
